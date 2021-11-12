f21598b6b2dd46d5bc0037088cdd2aa4b7848c5a
Justin Lee <jlee7@uccs.edu>
2021-11-12 23:13:53 +0000

    Readded title and description validation.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
-    #validates_presence_of :title, :description #User input validation that checks that the project is being created with a title and description.
+    # require title and description
+    validates_presence_of :title, :description #User input validation that checks that the project is being created with a title and description.
 end
